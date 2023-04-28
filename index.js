c02a9c57eb0a3939d0b98dd99aa3c74b17c4eb3e
tati-bueno <bueno.tati15@hotmail.com>
2023-04-27 22:21:38 -0300

    esse é meu terceiro commit

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("oi meu nome é tati")
+var nome="tati"
+function digameunome(){
+    console.log("oi meu nome é" + nome)
+
+}
+digameunome()
+function qualminhaidade(){
+    const idade=32
+    console.log(idade)
+}
+qualminhaidade()
